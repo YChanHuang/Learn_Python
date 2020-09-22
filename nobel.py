c2e13d11c174438b216406b004371fb5ef4cd269
AhhhHaaa <JamesHuang@users.noreply.github.com>
2020-09-22 11:11:09 +0100

    0922

---
+++ nobel.py
@@ -0,0 +1,9 @@
+# import relevant libraries
+import pandas as pd
+import seaborn as sns
+import numpy as np
+
+# read historical nobel winner data
+nobel = pd.read_csv('nobel_archive.csv')
+
+print(nobel)
