fe01586812a66fecf9d2a6306b8335d30cb66e43
YChanHuang <j3505151@gmail.com>
2020-09-22 22:19:58 +0100

    Sort out the files

---
+++ py4e_note.py
@@ -0,0 +1,17 @@
+    x = list()
+    x.append('1')
+    dir(x)
+    
+    print(x)
+    
+# split() function
+string.split() # split a string into a list default on space
+test_string = ' abc, so abc.split, and this returns a list'
+test_list = test_string.split()
+# test string as following example
+# ['abc,', 'so', 'abc.split,', 'and', 'this', 'returns', 'a', 'list']
+string.split(';') # split on specific character
+
+
+
+
